cb9cdd0b4f72a3fcf5993eabee082fd3c3cc972a
Anuj Ghimire <anujghimire41@gmail.com>
2026-03-31 21:48:52 +0545

    pushed time app to github

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [time, settime] = useState(0);
+  const [value, setvalue] = useState(false);
+
+  useEffect(() => {
+    let id;
+    if (value) {
+      id = setInterval(() => {
+        settime((t) => t + 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(id);
+    };
+  }, [value]);
+
+  return (
+    <>
+      <div className="time">{time}</div>
+
+      <div className="btncontainer">
+        <div className="start">
+          <button
+            className="btn"
+            onClick={() => {
+              setvalue(true);
+            }}
+          >
+            start
+          </button>
+        </div>
+        <div className="pause">
+          <button
+            className="btn"
+            onClick={() => {
+              setvalue(false);
+            }}
+          >
+            pause
+          </button>
+        </div>
+        <div className="reset">
+          <button
+            className="btn"
+            onClick={() => {
+              settime(0);
+              setvalue(false);
+            }}
+          >
+            reset
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
